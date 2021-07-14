5273664fb8df81a00f70b21e979334214df2de4c
Giovanni Prisco <gprisco01@gmail.com>
2021-07-14 09:53:32 +0200

    added graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gprisco/nic-series-yt/handlers"
 )
@@ -18,5 +21,38 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// Put The Server start in a goroutine, so it will not block
+	go func() {
+		log.Println("Server listening...")
+		err := s.ListenAndServe()
+
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	// Create a channel
+	sigChan := make(chan os.Signal, 1) // 1 -> buffer of size 1
+
+	// Notify sigChan everytime we get Interrupt or Kill Signal
+	signal.Notify(sigChan, os.Interrupt)
+
+	// !!! THIS IS BLOCKING -> will listen for signals (specified above by us)
+	sig := <-sigChan
+	logger.Println("Gracefully shutdown...", sig)
+
+	// Get Background context, assign a 30 seconds timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Shutdown the server with the timeout specified above
+	s.Shutdown(ctx)
 }
