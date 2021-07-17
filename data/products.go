af6aa2befc606fe0d2dce2c56eb5704f5841ca99
Giovanni Prisco <gprisco01@gmail.com>
2021-07-17 18:33:53 +0200

    Abstracted json encoding login into data.product

--- data/products.go
+++ data/products.go
@@ -1,39 +1,50 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	Price       float32
-	SKU         string
-	CreatedOn   string
-	UpdatedOn   string
-	DeletedOn   string
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description,omitempty"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
 }
 
-func GetProducts() []*Product {
+type Products []*Product
+
+func (p Products) ToJSON(w io.Writer) error {
+	encoder := json.NewEncoder(w)
+	return encoder.Encode(p)
+}
+
+func GetProducts() Products {
 	return productList
 }
 
-var productList = []*Product{
+var productList = Products{
 	{
 		ID:          1,
 		Name:        "Latte",
 		Description: "Frothy milk coffeee",
 		Price:       2.45,
 		SKU:         "abc323",
-		CreatedOn:   time.Now().String(),
-		UpdatedOn:   time.Now().String(),
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
 	},
 	{
 		ID:          2,
 		Name:        "Espresso",
-		Description: "Short and strong coffeee without milk",
+		Description: "",
 		Price:       1.99,
 		SKU:         "ajsnvnd",
-		CreatedOn:   time.Now().String(),
-		UpdatedOn:   time.Now().String(),
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
 	},
 }
--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
@@ -19,11 +18,9 @@ func NewProducts(l *log.Logger) *Products {
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 
 	if err != nil {
-		http.Error(rw, "Unable to Marshal json data", http.StatusInternalServerError)
+		http.Error(rw, "Unable to encode json data", http.StatusInternalServerError)
 	}
-
-	rw.Write(d)
 }
